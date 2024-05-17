93b10313fc883c10e2d8c75d4e203a93b7165c38
Ghadi Ayoub <121671297+Ghadi03@users.noreply.github.com>
2024-05-17 19:36:44 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("predict-cluster/", views.get_salary_cluster, name="predict-cluster"),
-    # path("analyze-description", views.job_description, name='analyze-description')
+    path("analyze-description", views.job_description, name='analyze-description')
 ]
